4e8c116c7cf89745eae8408fc494d5dbf10ad451
ryanhtanto <ryanherrtanto@gmail.com>
2022-04-25 15:21:02 +0700

    antiparallax

--- main.js
+++ main.js
@@ -75,38 +75,38 @@ gsap.to("#section1-1 .random0 .bgorang , #section1-1 .random0 .mask2", {
   }, 
 });
 
-gsap.to("#section1-1 .random0 .eva , #section1-1 .random0 .mask1 , #section1-1 .random0 .mask3", {
-  yPercent: -275,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random0",
-    pin:"#section1-1 .random0 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random0 .mask1 , #section1-1 .random0 .mask3", {
+//   yPercent: -275,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random0",
+//     pin:"#section1-1 .random0 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random0 .parallax", {
-  yPercent: -600,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random0",
-    pin:"#section1-1 .random0 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random0 .parallax", {
+//   yPercent: -600,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random0",
+//     pin:"#section1-1 .random0 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random0 .solusibtn", {
-  yPercent: -800,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random0",
-    pin:"#section1-1 .random0 .row",
-    start: "-10%",
-    scrub:0.01,
-  }, 
-});
+// gsap.to("#section1-1 .random0 .solusibtn", {
+//   yPercent: -800,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random0",
+//     pin:"#section1-1 .random0 .row",
+//     start: "-10%",
+//     scrub:0.01,
+//   }, 
+// });
 
 // random 1
 gsap.to("#section1-1 .random1 .panahlingkaran", {
@@ -141,38 +141,38 @@ gsap.to("#section1-1 .random1 .bgorang , #section1-1 .random1 .mask2", {
   }, 
 });
 
-gsap.to("#section1-1 .random1 .eva , #section1-1 .random1 .mask1 , #section1-1 .random1 .mask3", {
-  yPercent: -275,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random1",
-    pin:"#section1-1 .random1 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random1 .eva , #section1-1 .random1 .mask1 , #section1-1 .random1 .mask3", {
+//   yPercent: -275,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random1",
+//     pin:"#section1-1 .random1 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random1 .parallax", {
-  yPercent: -600,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random1",
-    pin:"#section1-1 .random1 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random1 .parallax", {
+//   yPercent: -600,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random1",
+//     pin:"#section1-1 .random1 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random1 .solusibtn", {
-  yPercent: -800,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random1",
-    pin:"#section1-1 .random1 .row",
-    start: "-10%",
-    scrub:0.01,
-  }, 
-});
+// gsap.to("#section1-1 .random1 .solusibtn", {
+//   yPercent: -800,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random1",
+//     pin:"#section1-1 .random1 .row",
+//     start: "-10%",
+//     scrub:0.01,
+//   }, 
+// });
 
 // random 2
 gsap.to("#section1-1 .random2 .panahlingkaran", {
@@ -207,38 +207,38 @@ gsap.to("#section1-1 .random2 .bgorang , #section1-1 .random2 .mask2", {
   }, 
 });
 
-gsap.to("#section1-1 .random2 .eva , #section1-1 .random2 .mask1 , #section1-1 .random2 .mask3", {
-  yPercent: -275,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random2",
-    pin:"#section1-1 .random2 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random2 .eva , #section1-1 .random2 .mask1 , #section1-1 .random2 .mask3", {
+//   yPercent: -275,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random2",
+//     pin:"#section1-1 .random2 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random2 .parallax", {
-  yPercent: -600,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random2",
-    pin:"#section1-1 .random2 .row",
-    start: "-10%",
-    scrub: true
-  }, 
-});
+// gsap.to("#section1-1 .random2 .parallax", {
+//   yPercent: -600,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random2",
+//     pin:"#section1-1 .random2 .row",
+//     start: "-10%",
+//     scrub: true
+//   }, 
+// });
 
-gsap.to("#section1-1 .random2 .solusibtn", {
-  yPercent: -800,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#section1-1 .random2",
-    pin:"#section1-1 .random2 .row",
-    start: "-10%",
-    scrub:0.01,
-  }, 
-});
+// gsap.to("#section1-1 .random2 .solusibtn", {
+//   yPercent: -800,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#section1-1 .random2",
+//     pin:"#section1-1 .random2 .row",
+//     start: "-10%",
+//     scrub:0.01,
+//   }, 
+// });
 
 /*section pelacakan*/
 const pelacakan = gsap.timeline();
