714cfa518745c584c3ac5483d82c5b8485b41888
yogi-masaji <yogimasaji02@gmail.com>
2022-04-28 13:59:05 +0700

    Section 6-7 (tambah random content)

--- redirect.js
+++ redirect.js
@@ -1,9 +1,9 @@
-// function redirectMobileHandler() {
-//     const width = Math.max(document.clientWidth || 0, window.innerWidth || 0);
-//     if (width < 600) {
-//     window.location = "https://link-hp.netlify.app";
-//     }
-// }
+function redirectMobileHandler() {
+    const width = Math.max(document.clientWidth || 0, window.innerWidth || 0);
+    if (width < 600) {
+    window.location = "https://link-hp.netlify.app";
+    }
+}
 
-// window.onload = redirectMobileHandler();
-// window.onresize = () => redirectMobileHandler();
+window.onload = redirectMobileHandler();
+window.onresize = () => redirectMobileHandler();
